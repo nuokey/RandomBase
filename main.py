669a87ef258c1692a10ab75731dbe2ec8ba5b485
Timo <66961498+nuokey@users.noreply.github.com>
2021-02-09 22:17:46 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,9 +7,17 @@ root = Tk()
 db = sqlite3.connect('data.db')
 cursor = db.cursor()
 
-cursor.execute('DELETE FROM users')
-# for i in range(20):
-#     cursor.execute(f'INSERT INTO users VALUES ()')
+cursor.execute('''CREATE TABLE IF NOT EXIST users (
+	id INT,
+	surname TEXT,
+	name TEXT,
+	lastname TEXT,
+	age INT,
+	birthday TEXT,
+	birthtown TEXT,
+	tele_number INT,
+	email TEXT
+)''')
 db.commit()
 
 root.mainloop()
